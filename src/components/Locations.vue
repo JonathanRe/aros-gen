359125eb72d5e1865dd5654c3b5af5c402a35e90
Jonathan Remmers <jona.remm@gmail.com>
2021-10-18 22:42:06 +0200

    add basic dice rolling method, test data binding

--- src/components/Locations.vue
+++ src/components/Locations.vue
@@ -5,20 +5,29 @@
       <li v-for="location in locations" :key="location.name">
         {{ location.name }}
       </li>
-      <p class="mt-4">{{ locations[0].name }}</p>
+      <p class="mt-4">{{ locations[locationIndex].name }}</p>
+      <button class="bg-indigo-500" @click="setLocationIndex()">Reroll</button>
     </ul>
   </div>
 </template>
 
 <script>
 import locationsData from "../db.json";
+import { roll } from "../modules/dice";
 
 export default {
   data() {
     return {
       locations: locationsData,
+      locationIndex: roll(12),
     };
   },
+  methods: {
+    setLocationIndex() {
+      this.locationIndex = roll(12);
+    },
+  },
+  mounted() {},
 };
 </script>
 
