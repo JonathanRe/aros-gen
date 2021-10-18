291e86ad2f965739880816d1715a5021cda83e08
Jonathan Remmers <jona.remm@gmail.com>
2021-10-18 22:01:28 +0200

    test getting json data into vue

---
+++ src/components/Locations.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="text-2xl">Locations</h1>
+    <ul>
+      <li v-for="location in locations" :key="location.name">
+        {{ location.name }}
+      </li>
+      <p class="mt-4">{{ locations[0].name }}</p>
+    </ul>
+  </div>
+</template>
+
+<script>
+import locationsData from "../db.json";
+
+export default {
+  data() {
+    return {
+      locations: locationsData,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
